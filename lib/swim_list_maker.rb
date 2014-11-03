b6a833ce8e75757aa6206b1d5832b4f680a01a6b
dcorrigan <dcorrigan@scribenet.com>
2014-11-03 09:19:14 -0500

    changed swim list maker to rely on element weight rather than obscure sorting rules

--- lib/swim_list_maker.rb
+++ lib/swim_list_maker.rb
@@ -13,13 +13,13 @@ class SwimListMaker
     make_table_instance_vars
     TEMPLATE.result(binding)
   end
-    
+
   def make_table_instance_vars
     organize_into_learning_lists
     sort_lists
     format_into_tables
   end
-    
+
   def organize_into_learning_lists
     list.each do |el, vals|
       next unless vals[:learning_category]
@@ -34,7 +34,7 @@ class SwimListMaker
       end
     end
   end
-      
+
   def drop_in_spacing_category(vals)
     prof = vals[:spacing_profile]
     vals[:spacing_category] = SPACING[prof][:type]
@@ -50,63 +50,47 @@ class SwimListMaker
   end
 
   def generate_instance_val cate
-    instance_variable_set("@#{cate}", {}) 
+    instance_variable_set("@#{cate}", {})
     self.class.send(:attr_accessor, "#{cate}")
   end
-    
+
   def sort_lists # keeping this separate since it will be a more advanced method eventually
     types.each do |type|
       sorted = instance_variable_get('@' + type).sort_by{ |k,v| k }
-      sorted = sort_by_scml_logic( sorted )
-      sorted = pull_heads_to_top( sorted ) unless list_is_all_heads?(type)
-      instance_variable_set('@' + type, sorted) 
+      sorted = sort_by_weight(sorted, type) if sorted.first[1][:weight]
+      instance_variable_set('@' + type, sorted)
     end
-  end
-      
-  def list_is_all_heads?(type)
-    %w(book chapter heads).any?{ |x| type.to_s.match(x) } 
-  end
-      
-  def pull_heads_to_top sorted 
-    heads = sorted.collect{ |item| item if item[1][:spacing_category] == :head }.compact
-#    require 'pry'; binding.pry if sorted.any?{ |item| item.first.match(/secbot/) }
-    %w((?<!bo)t h ah ahaft bh bhaft ch chaft dh dhaft 1h 2h 3h).reverse.each do |type|
-      head_vars = heads.collect{ |head| head if head.first.match(/#{type}$/) }.compact
-      next if head_vars.empty?
-      head_vars.each do |head_var|
-        ind = sorted.index(head_var)
-        sorted.insert(0, sorted.delete_at(ind))
-        heads.delete(head_var)
-      end
-    end
-    sorted
   end
 
-  def sort_by_scml_logic sorted
-    sorted.each_with_index do |item, ind|
-      item_name = item.first
-      %w(f l s o).reverse.each do |spacer|
-        variant = sorted.collect{ |name, info| [name, info] if name == item_name + spacer }.compact.first
-        next if variant.nil?
-        var_ind = sorted.index(variant)
-        sorted.insert(ind + 1, sorted.delete_at( var_ind ) )
+  def sort_by_weight(list, type)
+    weights = list.each_with_object({}) do |arr, weights|
+      w = arr[1][:weight]
+      if weights[w]
+        weights[w] << arr
+      else
+        weights[w] = []
+        weights[w] << arr
       end
     end
-    sorted
+    weights.keys.sort.map { |num| weights[num].sort_by { |n, v| n } }.flatten.each_slice(2).to_a
   end
-    
+
+  def list_is_all_heads?(type)
+    %w(book chapter heads).any?{ |x| type.to_s.match(x) }
+  end
+
   def format_into_tables
     types.each do |type|
       list = instance_variable_get('@' + type)
       formatted_list = iterate_and_format_list( list )
-      instance_variable_set('@' + type, formatted_list) 
+      instance_variable_set('@' + type, formatted_list)
     end
   end
 
   def iterate_and_format_list list
     list.map{ |el, vals|
       defin = vals[:name] || ''
-      tag = make_tag(el, vals)    
+      tag = make_tag(el, vals)
       rev = vals[:date] || ''
       "#{tag} | #{defin} | #{rev} "
     }.join("\n")
@@ -120,5 +104,4 @@ class SwimListMaker
   def constantize string
     string.downcase.gsub(/[ \/\-]/,'_').gsub(/[^a-z_]/, '')
   end
-
 end
